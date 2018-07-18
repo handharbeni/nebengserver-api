6ed34c0bd02e951beeff50f7a20bac7df6fe8d37
handharbeni <mhandharbeni@gmail.com>
2018-07-18 18:35:50 +0700

    Change Host

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
 // Constants
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // app.use(bodyParser.urlencoded({ extended: true }));
